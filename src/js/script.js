e719bc54c3df4d53e1360b677861afedd81e296a
verumiam <danil_grigoruk@mail.ru>
2022-07-24 15:11:41 +0300

    finished promo

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,33 @@
 window.addEventListener('DOMContentLoaded', () => {
     const animation = bodymovin.loadAnimation({
-        container: document.querySelector('#container'),
+        container: document.querySelector('#logo-video'),
         rederer: 'svg',
         loop: false,
         autoplay: true,
-        path: 'js/data2.json'
+        path: 'js/animation.json'
     });
+
+    // Header class on scroll
+    let header = document.querySelector('#header'),
+        nav = document.querySelector('#nav');
+
+    headerScroll();
+
+    window.addEventListener('scroll resize', function() {
+        headerScroll();
+    });
+
+    function headerScroll () {
+        window.addEventListener('scroll', function() {
+            let scrollH = this.scrollY,
+                headerH = header.scrollHeight,
+                navH = nav.scrollHeight;
+    
+            if (scrollH >= headerH + navH) {
+                nav.classList.add('nav-active');
+            } else {
+                nav.classList.remove('nav-active');
+            }
+        });
+    }
 });
