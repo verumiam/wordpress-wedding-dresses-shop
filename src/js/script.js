4a59509b367ccff26046526bd3ebe79923b826ed
verumiam <danil_grigoruk@mail.ru>
2022-08-09 14:02:38 +0300

    add shopping cart

--- src/js/script.js
+++ src/js/script.js
@@ -160,50 +160,227 @@ window.addEventListener('DOMContentLoaded', () => {
         
     // });
 
-    // Function to convert html table into json data
+    const cartDomElement = document.querySelector('.js-cart');
 
-    // function tableToJson(table) {
-    //     let data = [];
-    
-    //     // first row needs to be headers
-    //     let headers = [];
-    //     for (let i=0; i<table.rows[0].cells.length; i++) {
-    //         headers[i] = table.rows[0].cells[i].innerHTML.toLowerCase().replace(/ /gi,'');
-    //     }
-    
-    //     // go through cells
-    //     for (let i=1; i<table.rows.length; i++) {
-    
-    //         let tableRow = table.rows[i];
-    //         let rowData = {};
-    
-    //         for (let j=0; j<tableRow.cells.length; j++) {
-    
-    //             rowData[ headers[j] ] = tableRow.cells[j].innerHTML;
-    
-    //         }
-    
-    //         data.push(rowData);
-    //     }       
-    
-    //     return data;
-    // }
+    if(!cartDomElement) {
+        return;
+    }
 
-    // let sizeTable = JSON.stringify(tableToJson(document.querySelector('.sizetable')));
+    const cart = JSON.parse(localStorage.getItem('cart')) || {};
+    const cartItemsCounterDOMElement = document.querySelector('.js-cart-total-count-items');        
+    const cartTotalPriceDOMElement = document.querySelector('.js-cart-total-price');
+    const cartTotalPriceInputDOMElement = document.querySelector('.js-cart-total-price-input');
 
-    // function downloadObject(exportOdbj, exportName) {
-    //     let dataStr = "data: text/json;charset=utf-8," + encodeURIComponent(exportOdbj);
-    //     let downloadAnchor = document.createElement('a');
-    //     downloadAnchor.setAttribute("href", dataStr);
-    //     downloadAnchor.setAttribute("download", exportName + ".json");
-    //     document.body.appendChild(downloadAnchor);
-    //     downloadAnchor.click();
-    //     downloadAnchor.remove();
-    // }
+    const renderCartItem = ({id, name, attribute, src, price, quantity}) => {
+        const cartItemDOMElement = document.createElement('div');
 
-    // document.querySelector('.help__btn-size').onclick = function() {
-    //     downloadObject(sizeTable, 'sizetable');
-    // }
+        const attributeTemplate = attribute 
+        ? ` <p name="1-Размер" class="cart__item-attribute">EU: ${attribute}</p> <input type="hidden" name="${id}-Аттрибут" value="${attribute}"> ` 
+        : '';
+
+        const cartItemTemplate = `
+            <div class="cart-item cart__item">
+            <div class="cart__item-main d-flex justify-content-center flex-wrap align-items-center">
+                <div class="col-md-3">
+                    <div class="cart__item-start">
+                        <button type="button" class="cart__item-btn cart__item-btn--remove js-btn-cart__item-remove">Remove</button>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="cart__item-img-wrapper">
+                        <img src="${src}" alt="" class="cart__item-img">
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="cart__item-descr d-flex justify-content-center align-items-center">
+                        <div class="cart__item-title"><span class="cart__item-title-name">${name}</span></div>
+                        <input type="hidden" name="${id}-Товар" value="${name}">
+                        <input type="hidden" name="${id}-Количество" value="${quantity}">
+                        <input type="hidden" name="${id}-Цена" value="${price * quantity}">
+                        ${attributeTemplate}
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="cart__item-end">
+                        <div class="cart__item-actions d-flex flex-nowrap align-items-center justify-content-center">
+                            <button type="button" class="cart__item-btn-minus js-btn-product-dec-quantity"><img src="icons/minus.svg" alt=""></button>
+                            <span class="cart__item-quantity js-cart__item-quantity">${quantity}</span>
+                            <button type="button" class="cart__item-btn-plus js-btn-product-inc-quantity"><img src="icons/plus.svg" alt=""></button>
+                            <div class="cart__item-price"><span class="js-cart__item-price">${price * quantity}</span>&#8364</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        `;
+
+        cartItemDOMElement.innerHTML = cartItemTemplate;
+        cartItemDOMElement.setAttribute('data-product-id', id);
+        cartItemDOMElement.classList.add('js-cart-item');
+        cartDomElement.appendChild(cartItemDOMElement);
+    };
+
+    saveCart = () => {
+        localStorage.setItem('cart', JSON.stringify(cart));
+    };
+
+    const updateCartTotalPrice = () => {
+        const totalPrice = Object.keys(cart).reduce((acc, id) => {
+            const {quantity, price} = cart[id];
+            return acc + price * quantity;
+        }, 0);
+
+        if (cartTotalPriceDOMElement) {
+            cartTotalPriceDOMElement.textContent = totalPrice;
+        }
+
+        if (cartTotalPriceDOMElement) {
+            cartTotalPriceInputDOMElement.value = totalPrice;
+        }
+    }
+
+    const updateCartTotalItemsCounter = () => {
+        const totalQuantity = Object.keys(cart).reduce((acc, id) => {
+            const {quantity} = cart[id];
+            return acc + quantity;
+        }, 0);
+
+        if (cartItemsCounterDOMElement) {
+            cartItemsCounterDOMElement.textContent = totalQuantity;
+        }
+    }
+
+    const updateCart = () => {
+        console.log(cart);
+        updateCartTotalPrice();
+        updateCartTotalItemsCounter();
+        saveCart();
+    };
+
+    const deleteCartItem = (id) => {
+        const cartItemDOMElement = cartDomElement.querySelector(`[data-product-id="${id}"]`);
+
+        cartDomElement.removeChild(cartItemDOMElement);
+        delete cart[id];
+        updateCart();
+    };
+
+    const addCartItem = (data) => {
+        const {id} = data;
+
+        if (cart[id]) {
+            incQuantity(id);
+            return;
+        }
+
+        cart[id] = data;
+        renderCartItem(data);
+        updateCart();
+    };
+
+    const updateQuantity = (id, quantity) => {
+        const cartItemDOMElement = cartDomElement.querySelector(`[data-product-id="${id}"]`);
+        const cartItemQuantityDOMElement = cartItemDOMElement.querySelector('.js-cart__item-quantity');
+        const cartItemPriceDOMElement = cartItemDOMElement.querySelector('.js-cart__item-price');
+
+        cart[id].quantity = quantity;
+        cartItemQuantityDOMElement.textContent = quantity;
+        cartItemPriceDOMElement.textContent = quantity * cart[id].price;
+
+        updateCart();
+    }
+
+    const decQuantity = (id) => {
+        const newQuantity = cart[id].quantity - 1;
+        if (newQuantity >= 1) {
+            updateQuantity(id, newQuantity);
+        }
+    }
+
+    const incQuantity = (id) => {
+        const newQuantity = cart[id].quantity + 1;
+        
+        updateQuantity(id, newQuantity);
+    }
+
+    const generateID = (string1, string2) => {
+        const secondParam = string2 ? `-${string2}` : '';
+        return `${string1}${secondParam}`.replace(/ /g, '-');
+    };
+
+    const getProductData = (productDOMElement) => {
+        const name = productDOMElement.getAttribute('data-product-name');
+                const attribute = productDOMElement.getAttribute('data-product-attribute');
+                const price = productDOMElement.getAttribute('data-product-price');
+                const src = productDOMElement.getAttribute('data-product-src');
+                const quantity = 1;
+                const id = generateID(name, attribute);
+
+        return { name, attribute, price, src, quantity, id };
+    };
+
+    const renderCart = () => {
+        const ids = Object.keys(cart);
+        ids.forEach((id) => renderCartItem(cart[id]));
+    }
+
+    const cartInit = () => {
+        renderCart();
+        updateCart();
+
+        document.querySelector('body').addEventListener('click', (e) => {
+            const target = e.target;
+
+            if (target.classList.contains('js-btn-add-to-cart')) {
+                e.preventDefault();
+                const productDOMElement = target.closest('.js-product');
+                const data = getProductData(productDOMElement);
+                addCartItem(data);
+            }
+
+            if (target.classList.contains('js-btn-cart__item-remove')) {
+                e.preventDefault();
+                const cartItemDOMElement = target.closest('.js-cart-item');
+                const productID = cartItemDOMElement.getAttribute('data-product-id');
+                deleteCartItem(productID);
+            }
+
+            if (target.classList.contains('js-btn-product-inc-quantity')) {
+                e.preventDefault();
+                const cartItemDOMElement = target.closest('.js-cart-item');
+                const productID = cartItemDOMElement.getAttribute('data-product-id');
+                incQuantity(productID);
+            }
+
+            if (target.classList.contains('js-btn-product-dec-quantity')) {
+                e.preventDefault();
+                const cartItemDOMElement = target.closest('.js-cart-item');
+                const productID = cartItemDOMElement.getAttribute('data-product-id');
+                decQuantity(productID);
+            }
+
+            if (target.classList.contains('js-btn-product-attribute')) {
+                e.preventDefault();
+                const attribute = target.getAttribute('data-product-attribute-value');
+                const price = target.getAttribute('data-product-attribute-price');
+                const productDOMElement = target.closest('.js-product');
+                const activeAttributeDOMElement = productDOMElement.querySelector('.js-btn-product-attribute.size-active');
+                const productPriceDOMElement = productDOMElement.querySelector('.js-catalog__price-value');
+                const productChooseAttributeDOMElement = productDOMElement.querySelector('.js-product-attribute');
+
+                productPriceDOMElement.textContent = price;
+                productDOMElement.setAttribute('data-product-attribute', attribute);
+                productDOMElement.setAttribute('data-product-price', price);
+                activeAttributeDOMElement.classList.remove('size-active');
+                target.classList.add('size-active');
+                productChooseAttributeDOMElement.textContent = `(EU): ${attribute}`;
+
+            }
+        });
+    };
+
+    cartInit();
+
 });
 
 AOS.init({
